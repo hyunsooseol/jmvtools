0b900e4fe45c82b5a2b36b6b06ee0f12c7a9a454
Hyunsoo Seol <snow@cau.ac.kr>
2020-05-06 08:13:07 +0900

    Add files via upload

--- jmvtools.R
+++ jmvtools.R
@@ -1,7 +1,11 @@
 
 # install.packages('jmvtools', repos=c('https://repo.jamovi.org', 'https://cran.r-project.org'))
 
+# 1. Getting started-----------
+
 library(jmvtools)
+
+
 
 jmvtools::check(home='C:\\Users\\seol\\Documents\\jamovi')
 options(jamovi_home='C:\\Users\\seol\\Documents\\jamovi')
@@ -19,13 +23,16 @@ setwd("~/jmvbaseR-master")
 # 처음때만 실행, compile 기능을 수행함.
 jmvtools::install()   
 
-## Creating ttest module----------------------
+# 2. Creating ttest module----------------------
 
 setwd("~/jmvbaseR-master")
 
 jmvtools::create('SuperAwesome')
 
 setwd('SuperAwesome')
+
+jmvtools::install()
+
 
 # addAnalysis()
 # addAnalysis(name=ttest, title=independent)
@@ -36,6 +43,8 @@ jmvtools::install()
 
 
 ## Creating Correlation module---------
+
+setwd("~/jmvbaseR-master")
 
 jmvtools::create('Correlations')
 
@@ -50,10 +59,14 @@ jmvtools::addAnalysis(name='correlation', title='correlation')
 # see "*.a.yaml"
 # insert function into "*.b.R"
 
+# compiler mode: "tame" in UI file.
+# https://dev.jamovi.org/ui-basic-design.html
+
 jmvtools::install()
 
 # go to directory "Correlations", and side load "correlations_0.0.0..jmo"
 # in jamovi program
+# Exercising Reliability analysis module in jamovi
 
 
 
